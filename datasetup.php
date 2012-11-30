8530dfaf23053a41f80f7d267fc8e1f60d919911
Tomas <tomas@tomas-linux.(none)>
2012-11-30 12:54:19 -0500

    initial commit

--- datasetup.php
+++ datasetup.php
@@ -3,27 +3,14 @@
 $con = mysql_connect("localhost", "cats", "cats123!!");
 
 if (!$con)
-	echo "Error connecting to database: " . mysql_error();
+        die("Could not connect: " . mysql_error());
 
 mysql_select_db("catPics", $con);
 
-
-$cur = curl_init();
-curl_setopt($cur, CURLOPT_URL, 'imgur.com/r/cats.json');
-curl_setopt($cur, CURLOPT_RETURNTRANSFER, 1);
-$result = curl_exec($cur);
-curl_close($cur);
-
-echo "<!-- $result -->";
-$imgur = json_decode($result, true);
-
-foreach($imgur['data'] as $img)
-{
-	$imgUrl = "http://i.imgur.com/". $img['hash'].$img['ext'];
-	mysql_query("INSERT INTO catURLS (urls) VALUES ('$imgUrl')") or die(mysql_error());
-	echo $imgUrl;
-}
+$url = mysql_query("SELECT urls FROM catURLS ORDER BY RAND() LIMIT 1");
 
 mysql_close($con);
+
+echo mysql_result($url, 0, "urls");
 
 ?>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $con = mysql_connect("localhost", "cats", "cats123!!");
 
 if (!$con)
-	die("Could not connect: " . mysql_error());
+        die("Could not connect: " . mysql_error());
 
 mysql_select_db("catPics", $con);
 
@@ -11,31 +11,6 @@ $url = mysql_query("SELECT urls FROM catURLS ORDER BY RAND() LIMIT 1");
 
 mysql_close($con);
 
+echo mysql_result($url, 0, "urls");
+
 ?>
-
-<html>
-<head>
-
-<script src = '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'></script>
-<script>
-
-$(document).ready(function()
-{
-	$('#newCat').click(function() 
-	{
-		$.ajax("cat.php").done(function(data){
-	 		$("#catID").attr('src', data)
-		})	
-	});	
-});
-
-</script>
-</head>
-<body>
-
-<img src = "<?php echo mysql_result($url, 0, "urls"); ?>" id = "catID" height = 600 />
-<br>
-<button type = "button" id = "newCat" > New pic </button>
-
-</body>
-</html>
