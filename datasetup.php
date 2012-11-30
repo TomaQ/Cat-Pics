aa66b7dd3e56dbd711996e7287ea84aca0ee1a86
Tomas <tomas@tomas-linux.(none)>
2012-11-29 23:41:16 -0500

    initial commit

--- datasetup.php
+++ datasetup.php
@@ -1,16 +1,27 @@
 <?php
 
-$con = mysql_connect();
-if (!$con)
-	die("Could not connect: " . mysql_error());
+$con = mysql_connect("localhost", "cats", "cats123!!");
 
-if (mysql_query("CREATE DATABASE my_db", $con)
+if (!$con)
+	echo "Error connecting to database: " . mysql_error();
+
+mysql_select_db("catPics", $con);
+
+
+$cur = curl_init();
+curl_setopt($cur, CURLOPT_URL, 'imgur.com/r/cats.json');
+curl_setopt($cur, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($cur);
+curl_close($cur);
+
+echo "<!-- $result -->";
+$imgur = json_decode($result, true);
+
+foreach($imgur['data'] as $img)
 {
-	echo "Database has been created.";
-}
-else
-{
-	echo "Could not create database.";
+	$imgUrl = "http://i.imgur.com/". $img['hash'].$img['ext'];
+	mysql_query("INSERT INTO catURLS (urls) VALUES ('$imgUrl')") or die(mysql_error());
+	echo $imgUrl;
 }
 
 mysql_close($con);
