8b934b8d09ddf38eea78ccf208d4f41f7e1e83e3
tomaq <tomas@tomas-linux.(none)>
2012-11-30 13:04:26 -0500

    added title and alignment

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $con = mysql_connect("localhost", "cats", "cats123!!");
 
 if (!$con)
-        die("Could not connect: " . mysql_error());
+	die("Could not connect: " . mysql_error());
 
 mysql_select_db("catPics", $con);
 
@@ -11,6 +11,35 @@ $url = mysql_query("SELECT urls FROM catURLS ORDER BY RAND() LIMIT 1");
 
 mysql_close($con);
 
-echo mysql_result($url, 0, "urls");
-
 ?>
+
+<html>
+<head>
+
+<title> Random Cats </title>
+
+<script src = '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'></script>
+<script>
+
+$(document).ready(function()
+{
+	$('#newCat').click(function() 
+	{
+		$.ajax("cat.php?random="+Math.random()).done(function(data){
+	 		$("#catID").attr('src', data)
+		})	
+	});	
+});
+
+</script>
+</head>
+	<body>
+
+	<div align = "center">
+		<img src = "<?php echo mysql_result($url, 0, "urls"); ?>" id = "catID" height = 600 />
+		<br>
+		<button type = "button" id = "newCat" > New pic </button>
+	</div>
+
+	</body>
+</html>
