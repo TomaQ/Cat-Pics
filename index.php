78c3e4219326c0355f3b8050efead299a8a8f2b4
tomaq <tomas@tomas-linux.(none)>
2012-12-02 23:27:04 -0500

    made it so that everytime the page is visited it queries new images and adds to database

--- index.php
+++ index.php
@@ -7,6 +7,20 @@ if (!$con)
 
 mysql_select_db("catPics", $con);
 
+$cur = curl_init();
+curl_setopt($cur, CURLOPT_URL, 'imgur.com/r/cats.json');
+curl_setopt($cur, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($cur);
+curl_close($cur);
+
+$imgur = json_decode($result, true);
+
+foreach($imgur['data'] as $img)
+{
+	$imgUrl = "http://i.imgur.com/" . $img['hash'] . $img['ext'];
+	mysql_query("INSERT IGNORE INTO catURLS (urls) SELECT '$imgUrl'") or die(mysql_error());
+}
+//mysql_query("INSERT INRO ");
 $url = mysql_query("SELECT urls FROM catURLS ORDER BY RAND() LIMIT 1");
 
 mysql_close($con);
